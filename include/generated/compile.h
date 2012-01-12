2e818bf82db9d2b1549704c491f2388825e6c714
droidzone <droidstarburst@gmail.com>
2012-01-12 13:25:49 +0530

    First Commit ------------ This base is not a clean Htc Kernel base, but my modified version with various patches. CPU Freqs: conservative ondemand userspace powersave performance

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 2 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 PREEMPT Thu Jan 12 12:04:22 IST 2012"
+#define LINUX_COMPILE_TIME "12:04:22"
+#define LINUX_COMPILE_BY "droidzone"
+#define LINUX_COMPILE_HOST "supernova"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.6.2 (crosstool-NG-1.9.3) "
---
+++ usr/.initramfs_data.cpio.d
@@ -0,0 +1,1 @@
+deps_initramfs := /home/droidzone/android/kernel/bravo_2.6.35_gb-mr-working/scripts/gen_initramfs_list.sh \
