78dab9a721d50d5dcc9e4b6f9828d0b70f8a2db2
Droidzone <dz@droidzone.in>
2012-01-11 18:22:11 -0800

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 14 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#14 PREEMPT Wed Jan 11 13:21:48 PST 2012"
+#define LINUX_COMPILE_TIME "13:21:48"
+#define LINUX_COMPILE_BY "droidzone"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.6.2 (crosstool-NG-1.9.3) "
