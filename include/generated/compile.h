42475b0411acd0aa47a33c0da7b6a8a2d171f2a2
droidzone <droidzone@shell-24003.(none)>
2012-03-06 06:46:43 +0000

    Powersaver frequencies

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 18 */
+/* This file is auto generated, version 19 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#18 PREEMPT Thu Feb 2 10:50:39 UTC 2012"
-#define LINUX_COMPILE_TIME "10:50:39"
+#define UTS_VERSION "#19 PREEMPT Tue Mar 6 06:17:04 UTC 2012"
+#define LINUX_COMPILE_TIME "06:17:04"
 #define LINUX_COMPILE_BY "droidzone"
-#define LINUX_COMPILE_HOST "shell-21003"
+#define LINUX_COMPILE_HOST "shell-24003"
 #define LINUX_COMPILE_DOMAIN "(none)"
 #define LINUX_COMPILER "gcc version 4.6.2 (crosstool-NG 1.13.3) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.35.10_basekernel-gfb1198d-dirty"
+#define UTS_RELEASE "2.6.35.10_No2way-g419cd18-dirty"
